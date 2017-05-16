707354ffd961c6cdfbf242cc74d14bf4dc79bb37
Sung <sungmkim80@gmail.com>
2017-05-16 19:02:28 -0400

    Trying OOP in ES6

---
+++ app/calculator.js
@@ -0,0 +1,5 @@
+const add = (x, y) => x + y;
+const mult = (x, y) => x * y;
+
+export {add, mult};
+export default mult;
--- app/index.js
+++ app/index.js
@@ -1,3 +1,25 @@
-let ranger = {magical: false, power: 9};
-let {power, magical} = ranger;
-console.log(magical, power);
+class Entity {
+    constructor(name, height) {
+        this.name = name;
+        this.height = height;
+    }
+
+    greet() {
+        console.log(`Hi I'm ${this.name} from middle earth`);
+    }
+}
+
+let merry = new Entity("Merry", 4.6);
+merry.greet();
+
+
+// import {students} from "./students";
+// console.log("students: ", students);
+
+// import {add, mult} from "./calculator";
+// console.log(add(1, 2));
+// console.log(mult(1, 2));
+
+
+
+
